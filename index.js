c39f12c5bf0d3be83851dc49e4bcb44726a8f99e
Vanessa Tang <vanesst5@uci.edu>
2023-12-26 17:40:48 -0800

    Added summary stats

--- index.js
+++ index.js
@@ -67,19 +67,25 @@ addGamesToPage(GAMES_JSON)
 const contributionsCard = document.getElementById("num-contributions");
 
 // use reduce() to count the number of total contributions by summing the backers
-
+const individualContributions = GAMES_JSON.reduce( (contributions, game) => {
+    return contributions + game.backers;
+}, 0);
 
 // set the inner HTML using a template literal and toLocaleString to get a number with commas
-
+contributionsCard.innerHTML = `<p>${individualContributions.toLocaleString()}</p>`;
 
 // grab the amount raised card, then use reduce() to find the total amount raised
 const raisedCard = document.getElementById("total-raised");
+const totalRaised = GAMES_JSON.reduce( (pledged, game) => {
+    return pledged + game.pledged;
+}, 0);
 
 // set inner HTML using template literal
-
+raisedCard.innerHTML = `<p>$${totalRaised.toLocaleString()}</p>`;
 
 // grab number of games card and set its inner HTML
 const gamesCard = document.getElementById("num-games");
+gamesCard.innerHTML = `<p>${GAMES_JSON.length.toLocaleString()}</p>`
 
 
 /*************************************************************************************
